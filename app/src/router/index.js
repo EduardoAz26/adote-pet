00966d33f6c9b43b43a140a4bb11510c12ce8b79
Eduardo Azevedo <eduardo@anato.com.br>
2022-09-09 15:14:21 -0300

    alteração search

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -19,6 +19,14 @@ const routes = [
 		component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
 	},
 	{
+		path: '/busca-pet',
+		name: 'search',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import(/* webpackChunkName: "about" */ '../views/SearchView.vue')
+	  },
+	{
 		path: '/ajuda',
 		name: 'help',
 		// route level code-splitting
