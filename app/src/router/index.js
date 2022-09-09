301d0a227e833cc58016d2392496ded00ff82fc8
yourname <you@example.com>
2022-09-09 14:49:24 -0300

    tela search

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -27,6 +27,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/HelpView.vue')
   },
   {
+    path: '/busca-pet',
+    name: 'search',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/SearchView.vue')
+  },
+  {
     path: '/cadastro-pet',
     name: 'petRegistration',
     // route level code-splitting
---
+++ app/src/views/SearchView.vue
@@ -0,0 +1,23 @@
+<template>
+
+<v-text-field
+            filled
+            label="Pesquisar"
+            prepend-inner-icon="mdi-magnify"
+          ></v-text-field>
+
+
+</template>
+
+<script>
+
+    export default {
+      name: 'HomeView',
+      components: {
+        
+      },
+      data: () => ({
+        
+      }),
+    }
+    </script>
